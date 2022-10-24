c44f1fd34ddf4e2b6b701f0680eb660767064b49
YUPPIE PF <yuppie@nodies.org>
2022-10-24 14:14:42 -0500

    Remove promtail-config.yml from being tracked Update variables.py to write to 2 separate docker-compose.yml Add .dockerignore to root Update .gitignore Add in template docker-compose.yml files

--- variables.py
+++ variables.py
@@ -1,13 +1,17 @@
 import yaml
 import os
 
-run_prometheus = False
-run_loki = False
-run_provisoning_datasources = False
-run_contactpoint = False
-run_client_promtail = False
-run_client_bcexporter = False
-run_docker_compose = False
+from dotenv import load_dotenv
+load_dotenv()
+
+run_prometheus = True
+run_loki = True
+run_provisoning_datasources = True
+run_contactpoint = True
+run_client_promtail = True
+run_client_bcexporter = True
+run_docker_compose = True
+run_client_docker_compose = True
 
 template_prometheus = './templates/prometheus.yml'
 output_prometheus = './prometheus/prometheus.yml'
@@ -18,17 +22,20 @@ output_loki = './loki/loki-config.yml'
 template_datasource = './templates/datasources'
 output_datasource = './grafana_provisioning/datasources'
 
-template_contactpoint = './alerting/contactpoint.yaml'
+template_contactpoint = './templates/alerting/contactpoint.yaml'
 output_contactpoint = './grafana_provisioning/alerting/contactpoint.yaml'
 
-template_client_promtail = './temlpates/clients/promtail-config.yml'
+template_client_promtail = './templates/clients/promtail-config.yml'
 output_client_promtail = './clients/promtail/promtail-config.yml'
 
 template_client_bcexporter = './templates/clients/config.yml'
 output_client_bcexporter = './clients/bcexporter/config/config.yml'
 
-template_docker_compose = './docker-compose.yml'
+template_docker_compose = './templates/docker-compose.yml'
 output_docker_compose = './docker-compose.yml'
+
+template_client_docker_compose = './templates/clients/docker-compose.yml'
+output_client_docker_compose = './clients/docker-compose.yml'
 
 if run_prometheus:
     with open(template_prometheus, "r") as f:
@@ -37,11 +44,11 @@ if run_prometheus:
     for idx, scrape_config in enumerate(list_scrape_configs):
         job_name = scrape_config["job_name"]
         if job_name == 'node':
-            scrape_config["static_configs"][0]["targets"] = [os.getenv("NODE_EXPORTER_ENDPOINT") + ':' + os.getenv("NODE_EXPORTER_PORT")]
+            scrape_config["static_configs"][0]["targets"] = [os.getenv("LOGGING_ENDPOINT") + ':' + os.getenv("NODE_EXPORTER_PORT")]
         elif job_name == 'blockchain':
-            scrape_config["static_configs"][0]["targets"] = [os.getenv("BLOCKCHAIN_EXPORTER_ENDPOINT") + ':' + os.getenv("BLOCKCHAIN_EXPORTER_PORT")]
+            scrape_config["static_configs"][0]["targets"] = [os.getenv("LOGGING_ENDPOINT") + ':' + os.getenv("BLOCKCHAIN_EXPORTER_PORT")]
         elif job_name == 'cadvisor':
-            scrape_config["static_configs"][0]["targets"] = [os.getenv("CADVISOR_EXPORTER_ENDPOINT") + ':' + os.getenv("CADVISOR_EXPORTER_PORT")]
+            scrape_config["static_configs"][0]["targets"] = [os.getenv("LOGGING_ENDPOINT") + ':' + os.getenv("CADVISOR_EXPORTER_PORT")]
         else:
             print(f'Unknown prometheus job name {job_name}')
         list_scrape_configs[idx] = scrape_config
@@ -52,7 +59,7 @@ if run_prometheus:
 if run_loki:
     with open(template_loki, "r") as f:
         newdict = yaml.safe_load(f)
-    newdict["server"]["http_listen_port"] = os.getenv("LOKI_PORT")
+    newdict["server"]["http_listen_port"] = int(os.getenv("LOKI_PORT"))
     with open(output_loki, "w") as f:
         yaml.dump(newdict, f)
 
@@ -60,7 +67,7 @@ if run_provisoning_datasources:
     for source in ['loki', 'prometheus']:
         with open(f'{template_datasource}/{source}.yaml', "r") as f:
             newdict = yaml.safe_load(f)
-        newdict["datasources"][0]["url"] = os.getenv(f'{source.upper()}_ENDPOINT') + ':' + os.getenv(f'{source.upper()}_PORT')
+        newdict["datasources"][0]["url"] = os.getenv("MONITORING_ENDPOINT") + ':' + os.getenv(f'{source.upper()}_PORT')
         with open(f'{output_datasource}/{source}.yaml', "w") as f:
             yaml.dump(newdict, f)
 
@@ -74,15 +81,15 @@ if run_contactpoint:
 if run_client_promtail:
     with open(template_client_promtail, "r") as f:
         newdict = yaml.safe_load(f)
-    newdict["clients"][0]["url"] = os.getenv('LOKI_ENDPOINT') + ':' + os.getenv('LOKI_PORT') + '/loki/api/v1/push'
-    newdict["server"]["http_listen_port"] = os.getenv('PROMTAIL_PORT')
+    newdict["clients"][0]["url"] = os.getenv('MONITORING_ENDPOINT') + ':' + os.getenv('LOKI_PORT') + '/loki/api/v1/push'
+    newdict["server"]["http_listen_port"] = int(os.getenv('PROMTAIL_PORT'))
     with open(output_client_promtail, "w") as f:
         yaml.dump(newdict, f)
 
 if run_client_bcexporter:
     with open(template_client_bcexporter, "r") as f:
         newdict = yaml.safe_load(f)
-    newdict["exporter_port"] = os.getenv('BLOCKCHAIN_EXPORTER_PORT')
+    newdict["exporter_port"] = int(os.getenv('BLOCKCHAIN_EXPORTER_PORT'))
     with open(output_client_bcexporter, "w") as f:
         yaml.dump(newdict, f)
 
@@ -91,10 +98,21 @@ if run_docker_compose:
         newdict = yaml.safe_load(f)
     list_services = newdict["services"]
     for (k, v) in list_services.items():
-        if k not in ('loki', 'grafana', 'minio', 'prometheus', 'alertmanager', 'blockchain_exporter', 'promtail', 'node_exporter', 'cadvisor'):
+        if k not in ('loki', 'grafana', 'minio', 'prometheus', 'alertmanager'):
             print(f'Unknown service {k} in docker-compose')
             continue
         list_services[f'{k}']['ports'] = [os.getenv(f'{k.upper()}_PORT') + ':' + os.getenv(f'{k.upper()}_PORT')]
     with open(output_docker_compose, "w") as f:
         yaml.dump(newdict, f)
 
+if run_client_docker_compose:
+    with open(template_client_docker_compose, "r") as f:
+        newdict = yaml.safe_load(f)
+    list_services = newdict["services"]
+    for (k, v) in list_services.items():
+        if k not in ('blockchain_exporter', 'promtail', 'node_exporter', 'cadvisor'):
+            print(f'Unknown service {k} in clients/docker-compose')
+            continue
+        list_services[f'{k}']['ports'] = [os.getenv(f'{k.upper()}_PORT') + ':' + os.getenv(f'{k.upper()}_PORT')]
+    with open(output_client_docker_compose, "w") as f:
+        yaml.dump(newdict, f)
