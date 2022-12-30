176f799a31a2727ef6e7bb28628cc30e67451264
Musa <musa.yassin@gmail.com>
2022-12-30 00:08:22 -0400

    Add Near blockchain connector

---
+++ clients/bcexporter/connectors/NearConnector.py
@@ -0,0 +1,81 @@
+import asyncio
+import json
+import traceback
+import urllib.parse
+
+import aiohttp
+
+from appmetrics.AppMetrics import AppMetrics
+from connectors.ChainUrl import ChainUrl
+from connectors.Web3Connector import Web3Connector
+from data.ChainSyncStatus import ChainSyncStatus
+
+
+class NearConnector(Web3Connector):
+
+    def __init__(self, chain_url_obj: ChainUrl, destination: AppMetrics, id: str, request_kwargs=None):
+        self.id = id
+        self.chain_url_obj = chain_url_obj
+        self.labels = [id, str(chain_url_obj)]
+        self.destination = destination
+
+    async def get_sync_data(self) -> dict:
+        # Returns dict if currently syncing, otherwise returns False
+        async with aiohttp.ClientSession() as async_session:
+            response = await async_session.post(
+                timeout=5,
+                url=self.chain_url_obj.get_endpoint(),
+                json={
+                    "jsonrpc": "2.0",
+                    "id": "dontcare",
+                    "method": "status",
+                    "params": []
+                },
+                headers={"content-type": "application/json"}
+            )
+            json_object = json.loads((await response.content.read()).decode("utf8"))
+            sync_info = json_object["result"]["sync_info"]
+            sync_dict = {
+                "current_block": sync_info["latest_block_height"],
+                "latest_block": sync_info["latest_block_height"],
+                "status": ChainSyncStatus.SYNCING if sync_info["syncing"] else ChainSyncStatus.SYNCED
+            }
+            return sync_dict
+
+    async def get_current_block(self) -> int:
+        return -1
+
+    async def get_latest_block(self) -> int:
+        """
+        Latest block is same as current block whenever node is synced.
+        We can later replace this with a value from an explorer or an altruist if needed.
+        """
+        return await self.get_current_block()
+
+    async def report_metrics(self):
+        """
+        Get metrics from node and refresh Prometheus metrics with
+        new values.
+        """
+        try:
+            sync_data = await self.get_sync_data()
+            curr_height = sync_data["current_block"]
+            latest_height = sync_data["latest_block"]
+            self.destination.curr_height.labels(*self.labels).set(curr_height)
+            self.destination.latest_height.labels(*self.labels).set(latest_height)
+            self.destination.sync_status.labels(*self.labels).set(sync_data["status"])
+            print(f"{self.chain_url_obj.get_endpoint()} sent metrics for chain {self.id}")
+            print(f'Status: {sync_data["status"]}\nCurrent Height: {curr_height}\nLatest Height: {latest_height}')
+        except (asyncio.TimeoutError) as e:
+            print(f'Timeout Exception with: {self.chain_url_obj.get_endpoint()}')
+            traceback.print_exc()
+            self.destination.sync_status.labels(*self.labels).set(ChainSyncStatus.STOPPED)
+            self.destination.curr_height.labels(*self.labels).set(0)
+            self.destination.latest_height.labels(*self.labels).set(0)
+        except Exception as e:
+            print(f'Exception with: {self.chain_url_obj.get_endpoint()}')
+            traceback.print_exc()
+            #temporary until graphs can handle UNKNOWN
+            self.destination.sync_status.labels(*self.labels).set(ChainSyncStatus.STOPPED)
+            self.destination.curr_height.labels(*self.labels).set(0)
+            self.destination.latest_height.labels(*self.labels).set(0)
--- clients/bcexporter/connectors/connector_utils.py
+++ clients/bcexporter/connectors/connector_utils.py
@@ -3,6 +3,7 @@ from connectors.AvaxConnector import AvaxConnector
 from connectors.ChainUrl import ChainUrl
 from connectors.EthConnector import EthConnector
 from connectors.TendermintConnector import TendermintConnector
+from connectors.NearConnector import NearConnector
 from data.AvaxChainID import AvaxChainID
 from data.PoktChainID import PoktChainID
 from config.Config import Config
@@ -29,6 +30,10 @@ def create_connectors(appmetrics: AppMetrics, chains) -> list:
         elif chain["id"] == PoktChainID.DFK:
             connectors.append(AvaxConnector(chain_url_obj=chain_url_obj, destination=appmetrics, id=chain["id"],
                                             chain=AvaxChainID.DFK))
+
+        elif chain["id"] == PoktChainID.NEAR:
+            connectors.append(NearConnector(chain_url_obj=chain_url_obj, destination=appmetrics, id=chain["id"]))
+
         else:
             connectors.append(EthConnector(chain_url_obj=chain_url_obj, destination=appmetrics, id=chain["id"]))
 
